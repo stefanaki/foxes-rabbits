13d814729ebcb45848df1ddea824af0a7d25bd6b
Nelson Trindade <48919500+nelsontr@users.noreply.github.com>
2022-03-13 00:10:01 +0000

    Update worldgen.h

--- include/worldgen.h
+++ include/worldgen.h
@@ -1,13 +1,12 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "worldstructs.h"
+#include "cell.h"
 
 #ifndef WORLDGEN_LIB_H__
 #define WORLDGEN_LIB_H__
 
 float r4_uni(uint32_t *);
 void generate_element(int, char, uint32_t *, Board *);
-void insert_animal(int, int, char, Board *);
-bool position_empty(int, int, Board *);
 
 #endif /* WORLDGEN_LIB_H__ */
