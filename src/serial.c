64fc437fe080703f028b95aa2e785d0a1e31775c
Giorgos Stefanakis <stefgs99@gmail.com>
2022-03-16 00:31:54 +0200

    Debugging

--- src/serial.c
+++ src/serial.c
@@ -1,6 +1,7 @@
 #include "serial.h"
 #include "world.h"
 #include <stddef.h>
+#include <stdio.h>
 
 extern uint32_t generations;
 extern uint32_t M;
@@ -20,31 +21,35 @@ bool fox_sees_rabbit(Cell *cell, char type) {
 Cell *compute_next_position(World *world, int i, int j, char animal_type) {
   int C = i * N + j;
   int p = 0;
-  int available_cells[4] = {false, false, false,
+  bool available_cells[4] = {false, false, false,
                             false}; // top, right, bottom, left
 
   if (i - 1 >= 0 && world->grid[i - 1][j].type == EMPTY) {
     ++p;
     available_cells[0] = true;
-  } else if (i - 1 >= 0 && fox_sees_rabbit(&world->grid[i - 1][j], animal_type))
+  }
+  if (i - 1 >= 0 && fox_sees_rabbit(&world->grid[i - 1][j], animal_type))
     return &world->grid[i - 1][j];
 
   if (j + 1 < N && world->grid[i][j + 1].type == EMPTY) {
     ++p;
-    available_cells[0] = true;
-  } else if (j + 1 < N && fox_sees_rabbit(&world->grid[i][j + 1], animal_type))
+    available_cells[1] = true;
+  } 
+  if (j + 1 < N && fox_sees_rabbit(&world->grid[i][j + 1], animal_type))
     return &world->grid[i][j + 1];
 
   if (i + 1 < M && world->grid[i + 1][j].type == EMPTY) {
     ++p;
-    available_cells[1] = true;
-  } else if (i + 1 < M && fox_sees_rabbit(&world->grid[i + 1][j], animal_type))
+    available_cells[2] = true;
+  } 
+  if (i + 1 < M && fox_sees_rabbit(&world->grid[i + 1][j], animal_type))
     return &world->grid[i + 1][j];
 
   if (j - 1 >= 0 && world->grid[i][j - 1].type == EMPTY) {
     ++p;
-    available_cells[2] = true;
-  } else if (j - 1 >= 0 && fox_sees_rabbit(&world->grid[i][j - 1], animal_type))
+    available_cells[3] = true;
+  }
+  if (j - 1 >= 0 && fox_sees_rabbit(&world->grid[i][j - 1], animal_type))
     return &world->grid[i][j - 1];
 
   if (p == 0)
@@ -52,15 +57,16 @@ Cell *compute_next_position(World *world, int i, int j, char animal_type) {
 
   int res = C % p, idx = 0;
   while (idx < res) {
-    if (!available_cells[idx])
+    printf("res: %d\tidx: %d\n", res, idx);
+    if (available_cells[idx])
       ++idx;
   }
 
-  if (idx == 0)
+  if (idx == 1)
     return &world->grid[i - 1][j];
-  else if (idx == 1)
-    return &world->grid[i][j + 1];
   else if (idx == 2)
+    return &world->grid[i][j + 1];
+  else if (idx == 3)
     return &world->grid[i + 1][j];
   else
     return &world->grid[i][j - 1];
@@ -70,7 +76,7 @@ void resolve_conflicts(Cell *cell) {
   // Function that resolves conflicts that might appear on a cell
   Animal *current, *incoming;
 
-  if (cell->type == EMPTY)
+  if (!cell || cell->type == EMPTY)
     return;
 
   current = cell->animal;
@@ -137,41 +143,65 @@ void serial_implementation(World *world) {
   int i, j, gen, turn, col_offset;
   Cell *initial_pos = NULL, *landing_pos = NULL;
 
+  printf("ok\n");
+
   for (gen = 0; gen < generations; ++gen) {
     for (turn = 0; turn < 2; ++turn) {
       col_offset = turn;
+      printf("%d\n", turn);
       for (i = 0; i < M; ++i) {
         for (j = col_offset; j < N; j += 2) {
+          printf("%d\t%d\n", i, j);
           initial_pos = &world->grid[i][j];
+
+          if (initial_pos->type == EMPTY)
+            continue;
+
+          printf("animal type: %d\n", initial_pos->animal->type);
+
           if (initial_pos->modified_by_red || !initial_pos->animal) {
             initial_pos->modified_by_red = false;
             continue;
           }
 
           if (starvation_status(initial_pos->animal)) {
+            printf("time to die\n");
             kill_animal(initial_pos);
             continue;
           }
 
+          printf("ok until here\n");
+
           landing_pos =
               compute_next_position(world, i, j, initial_pos->animal->type);
+
+          if (landing_pos)
+            printf("ok next pos\npos_animal: %d\n", landing_pos->type);
 
           if (landing_pos != NULL) {
             landing_pos->incoming_animal = initial_pos->animal;
             initial_pos->modified_by_red = true;
 
-            move_animal(initial_pos, landing_pos);
+            // move_animal(initial_pos, landing_pos); // why???
+            initial_pos->type = EMPTY;
+            initial_pos->animal = NULL;
+
           } else { // Animal stays in current cell
             initial_pos->animal->breeding_age++;
           }
 
-          if (animal_type(landing_pos->incoming_animal, FOX))
+          printf("okokokok\n");
+
+          if (landing_pos && animal_type(landing_pos->incoming_animal, FOX))
             landing_pos->incoming_animal->starvation_age++;
+
+          printf("okokokok1234\n");
+
+          resolve_conflicts(initial_pos);
+          resolve_conflicts(landing_pos);
         }
         col_offset = col_offset == 0 ? 1 : 0;
       }
-      resolve_conflicts(initial_pos);
-      resolve_conflicts(landing_pos);
     }
   }
 }
