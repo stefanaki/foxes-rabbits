e37201e7278c5d8a24594a05a8a70afc934b75d5
Giorgos Stefanakis <stefgs99@gmail.com>
2022-04-12 02:55:46 +0300

    MPI driver code

--- src/foxes-rabbits-mpi.c
+++ src/foxes-rabbits-mpi.c
@@ -6,7 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define RANK_MASTER 0
+#define BLOCK_LOW(id, p, n) ((id) * (n) / (p))
+#define BLOCK_HIGH(id, p, n) (BLOCK_LOW((id) + 1, p, n) - 1)
+#define BLOCK_SIZE(id, p, n) (BLOCK_HIGH(id, p, n) - BLOCK_LOW(id, p, n) + 1)
+#define BLOCK_OWNER(j, p, n) (((p) * ((j) + 1) - 1) / (n))
+
+#define MASTER_RANK 0
 
 uint32_t generations;
 uint32_t M;
@@ -18,11 +23,6 @@ uint32_t rabbit_breeding;
 uint32_t fox_breeding;
 uint32_t fox_starvation;
 uint32_t seed;
-
-#define BLOCK_LOW(id, p, n) ((id) * (n) / (p))
-#define BLOCK_HIGH(id, p, n) (BLOCK_LOW((id) + 1, p, n) - 1)
-#define BLOCK_SIZE(id, p, n) (BLOCK_HIGH(id, p, n) - BLOCK_LOW(id, p, n) + 1)
-#define BLOCK_OWNER(j, p, n) (((p) * ((j) + 1) - 1) / (n))
 
 void print_board(Cell **grid, int chunk)
 {
@@ -107,14 +107,108 @@ Cell **generate_world_subgrid(int rank, int procs)
   generate_element_mpi(n_rabbits, RABBIT, &seed, subgrid, rank, procs);
   generate_element_mpi(n_foxes, FOX, &seed, subgrid, rank, procs);
 
-  printf("I am process %d. My allocated rows are:\n", rank);
-  print_board(subgrid, chunk);
   return subgrid;
 }
 
+// Compute next position modified
+void compute_next_position(Cell **grid, int i, int j, int rank, int procs, int *landing_pos);
+
 // Ask for data from cell that doesn't belong in the same process
+Cell get_cell_from_rank(int rank, int i, int j);
+
 // Modify cell that doesn't belong to the same process
+void modify_cell_from_rank(int rank, int i, int j, Cell data);
+
+// Resolve conflicts modified
+void resolve_conflicts(Cell *cell);
+
+// Send generation result to master
+void send_result_to_master(Cell **grid, int rank, int procs, int *totals[]);
+
 // Implementation and process reduction
+void mpi_implementation(Cell **grid, int rank, int procs)
+{
+  bool col_offset;
+  int i, j, gen, turn;
+  int landing_pos[2];
+  int total_elements[3] = {0, 0, 0}; // foxes, rabbits, rocks
+
+  for (gen = 0; gen < generations; ++gen)
+  {
+    for (turn = 0; turn < 2; ++turn)
+    {
+      col_offset = turn;
+      for (i = 0; i < BLOCK_SIZE(rank, procs, M); ++i)
+      {
+        for (j = col_offset; j < N; j += 2) {
+          if (grid[i][j].type != ANIMAL) continue;
+          else if (turn && grid[i][j].animal->modified_by_red)
+          {
+            grid[i][j].animal->modified_by_red = false;
+            continue;
+          }
+
+          landing_pos[0] = landing_pos[1] = -1;
+          compute_next_position(grid, i, j, rank, procs, &landing_pos);
+
+          grid[i][j].animal->starvation_age++;
+          grid[i][j].animal->breeding_age++;
+
+          if (landing_pos[0] != -1 && landing_pos[1] != -1)
+          {
+            grid[i][j].animal->modified_by_red = !turn;
+
+            if (breeding_status(grid[i][j].animal))
+            {
+              Animal *aux = create_animal(grid[i][j].type);
+              aux->modified_by_red = !turn;
+
+              change_breeding_age(grid[i][j].animal, 0);
+              grid[i][j].incoming_animals[grid[i][j].new_animals++] = aux;
+            }
+
+            if (rank == BLOCK_OWNER(landing_pos[0], procs, M))
+            {
+              grid[landing_pos[0]][landing_pos[1]].incoming_animals[grid[landing_pos[0]][landing_pos[1]].new_animals++] = grid[i][j].animal;
+              modify_cell(&grid[i][j], EMPTY, NULL);
+            } else
+            {
+              // tbh i need to think about this more
+              // Cell temp = get_cell_from_rank(BLOCK_OWNER(landing_pos[0], procs, M), landing_pos[0], landing_pos[1]);
+              // move_animal_to_cell();
+            }
+          }
+        }
+      }
+
+      for (int k = 0; k < BLOCK_SIZE(rank, procs, M); ++k)
+      {
+        for (int l = 0; l < N; ++l) {
+          if (grid[k][l].type != ROCK)
+          {
+            resolve_conflicts(&grid[k][l]);
+            grid[k][l].new_animals = 0;
+
+            if (turn && grid[k][l].animal)
+            {
+              if (starvation_status(&grid[k][l].animal))
+                kill_animal(&grid[k][l]);
+
+              if (grid[k][l].animal)
+                grid[k][l].animal->modified_by_red = false;
+            }
+          }
+        }
+      }
+    }
+
+    send_result_to_master(grid, rank, procs, &total_elements);
+  }
+
+  if (rank == MASTER_RANK)
+    output_final_population(&total_elements);
+}
+
 // Print result
 
 int main(int argc, char **argv)
